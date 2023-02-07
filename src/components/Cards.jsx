4b3822447c409dddf6bba2c90d6e29de098f30e9
mazufik <taufikprincecode10@gmail.com>
2023-02-07 16:55:59 +0900

    fix bug on component cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,12 +9,7 @@ const Cards = () => {
   return (
     <div className='w-full bg-[#d4f0fc] py-[10rem] px-4 text-[#01303f]'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'
-        >
+        <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
           <img
             className='w-20 mx-auto mt-[-3rem] bg-[#d4f0fc]'
             src={Single}
@@ -42,75 +37,65 @@ const Cards = () => {
           <button className='border border-[#02a9f7] hover:bg-[#02a9f7] w-[200px] rounded-md font-medium my-6 mx-auto py-3 hover:text-[#01303f] hover:scale-95 ease-linear duration-300'>
             Start Trial
           </button>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className='w-full bg-[#c4e7f7] shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'
-        >
+        </div>
+        <div className='w-full bg-[#c4e7f7] shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
           <img
             className='w-20 mx-auto mt-[-3rem] bg-transparent'
             src={Double}
             alt='Double'
           />
-          <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+          <h2 className='text-2xl font-bold text-center py-8'>Double User</h2>
           <NumericFormat
             className='text-center text-4xl font-bold'
-            value={149}
+            value={265}
             displayType={'text'}
             thousandSeparator={true}
             prefix={'$'}
           />
           <div className='text-center font-medium'>
             <p className='py-2 border-b border-slate-300 mx-8 mt-8'>
-              500 GB Storage
+              1024 GB Storage
             </p>
             <p className='py-2 border-b border-slate-300 mx-8'>
-              1 Granted User
+              4 Granted User
             </p>
             <p className='py-2 border-b border-slate-300 mx-8'>
-              Send up to 2 GB
+              Send up to 3 GB
             </p>
           </div>
           <button className='bg-[#02a9f7] hover:bg-[#89d6fb] w-[200px] rounded-md font-medium my-6 mx-auto py-3 hover:text-[#01303f] hover:scale-95 ease-linear duration-300'>
             Start Trial
           </button>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'
-        >
+        </div>
+        <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
           <img
             className='w-20 mx-auto mt-[-3rem] bg-[#d4f0fc]'
             src={Triple}
             alt='Triple'
           />
-          <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+          <h2 className='text-2xl font-bold text-center py-8'>Triple User</h2>
           <NumericFormat
             className='text-center text-4xl font-bold'
-            value={149}
+            value={299}
             displayType={'text'}
             thousandSeparator={true}
             prefix={'$'}
           />
           <div className='text-center font-medium'>
             <p className='py-2 border-b border-slate-300 mx-8 mt-8'>
-              500 GB Storage
+              2024 GB Storage
             </p>
             <p className='py-2 border-b border-slate-300 mx-8'>
-              1 Granted User
+              6 Granted User
             </p>
             <p className='py-2 border-b border-slate-300 mx-8'>
-              Send up to 2 GB
+              Send up to 4 GB
             </p>
           </div>
           <button className='border border-[#02a9f7] hover:bg-[#02a9f7] w-[200px] rounded-md font-medium my-6 mx-auto py-3 hover:text-[#01303f] hover:scale-95 ease-linear duration-300'>
             Start Trial
           </button>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
